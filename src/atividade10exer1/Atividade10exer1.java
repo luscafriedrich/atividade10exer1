af0c6187d07208bf7068051ca0e30bbf3c24dd9d
Lucas Friedrich <ntc.lucassilva@gmail.com>
2021-06-06 11:53:40 -0300

    Projeto criado dia 06/06/21 - Exercicio lógica, subindo para Github...

---
+++ src/atividade10exer1/Atividade10exer1.java
@@ -0,0 +1,12 @@
+package atividade10exer1;
+/**
+ *
+ * @author Lucas
+ */
+public class Atividade10exer1 {
+    public static void main(String[] args) {
+        for(int numero = 101; numero < 200; numero = numero + 2){
+            System.out.println("Números: "+numero);        
+    }  
+}
+}
